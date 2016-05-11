9cc48a398864efd76412108fbedc3a497d2c7b58
Àlvar Pérez <alvarperez92@gmail.com>
2016-05-12 00:03:03 +0200

    Modified populate file, checked roleMappings

--- README.md
+++ README.md
@@ -3,10 +3,7 @@
 A repository for reproducing [LoopBack community issues][wiki-issues].
 
 
-
-<h4 style="text-align:center">
-Repository created in order to solve the issue [#2256][loopback-issue]
-</h4>
+#### Repository created in order to solve the issue [#2256][loopback-issue]
 
 [wiki-issues]: https://github.com/strongloop/loopback/wiki/Reporting-issues
 [loopback-issue]: https://github.com/strongloop/loopback/issues/2256
--- server/boot/populate.js
+++ server/boot/populate.js
@@ -1,76 +1,66 @@
 var async = require('async');
+var fs = require('fs');
 
-module.exports = function(app) {
+module.exports = function (app) {
 
-    console.log('here');
+    var dataBasepath = './server/boot/populate-data/';
+
+    var userData = JSON.parse(fs.readFileSync(dataBasepath + 'user-data.json', 'utf8'));
+    var randomModelData = JSON.parse(fs.readFileSync(dataBasepath + 'random-model-data.json', 'utf8'));
+    var rolesData = JSON.parse(fs.readFileSync(dataBasepath + 'roles-data.json', 'utf8'));
+    var roleMappingData = JSON.parse(fs.readFileSync(dataBasepath + 'role-mapping-data.json', 'utf8'));
+
     async.series([
-        function(callback){
+        function (callback) {
             app.models.user.findOne({
                 _id: "5730f6b82eca2a182c717fcb"
-            }, function(err, found) {
-                if(err || found) return callback(err);
+            }, function (err, found) {
+                if (err || found) return callback(err);
 
-                app.models.user.create({
-                    "_id": "5730f6b82eca2a182c717fcb",
-                    "password": "$2a$10$zq2QxxlmJW5zK78PWUY4eeZnD1bHDWzZ91QKeO59jGJIDTmE/M/au",
-                    "email": "name@example.com"
-                }, function(err) {
+                app.models.user.upsert(userData, function (err) {
                     console.log('Created user with email: name@example.com and password: 123456');
-                    console.log('Err', err);
                     callback(err);
                 });
             });
         },
-        function(callback){
-            console.log('here 3');
-            app.models.RandomModel.create([
-                {
-                    "_id": "5730fadbc6c720700ecadb30",
-                    "name": "Express"
-                },
-                {
-                    "_id": "5730fae1c6c720700ecadb31",
-                    "name": "LoopBack"
-                }
-            ], function(err) {
+        function (callback) {
+            async.each(randomModelData, function (model, callback) {
+                app.models.RandomModel.upsert(model, function (err) {
+                    callback(err);
+                });
+            }, function (err) {
+                callback(err);
+            })
+        },
+        function (callback) {
+            async.each(rolesData, function (role, callback) {
+                app.models.Role.upsert(role, function (err) {
+                    callback(err);
+                });
+            }, function (err) {
                 callback(err);
             });
         },
         function (callback) {
-            console.log('here 4');
-            app.models.Role.create([
-                {
-                    "_id" : "571dd32a8cea6408067e8df3",
-                    "name" : "randomModel_findById"
-                },
-                {
-                    "_id" : "571dd3358cea6408067e8df4",
-                    "name" : "admin"
-                }
-            ], function (err) {
-                callback(err);
-            });
-        },
-        function (callback) {
-            console.log('here 5');
-            app.models.RoleMapping.create([
-                {
-                    "_id" : "571dd99a25dea8502e5aaba4",
-                    "principalType" : "ROLE",
-                    "principalId" : "571dd3358cea6408067e8df4",
-                    "roleId" : "571dd32a8cea6408067e8df3"
-                },
-                {
-                    "_id" : "571ddbe50e0c2a84166d2c79",
-                    "principalType" : "USER",
-                    "principalId" : "5718bbc2c134dce41909e74b",
-                    "roleId" : "571dd3358cea6408067e8df4"
-                }
-            ], function (err) {
+            async.each(roleMappingData, function (mapping, callback) {
+                app.models.RoleMapping.upsert(roleMappingData, function (err) {
+                    callback(err);
+                });
+            }, function (err) {
                 callback(err);
             });
         }, function (callback) {
-            console.log("DB Populated");
+            // Checking if user has the roles
+            app.models.Role.getRoles({
+                    principalType: app.models.RoleMapping.USER,
+                    principalId: '5730f6b82eca2a182c717fcb'
+                },
+                function (err, roles) {
+                    console.log('Roles', roles);  // everyone, authenticated, etc (hopefully)
+                    callback(err);
+                });
+        }, function (callback) {
+            console.log('DB Populated');
             callback();
         }
     ]);
